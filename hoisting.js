6190293e5e8e56f3eee331df4280ea06e5e42f2d
Nathan Boehringer <nathan@linebreak.dev>
2020-01-10 14:33:24 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,39 @@
-function callMe() {
+var callMe = "maybe";
+var lyric = callMe;
+var crazy = "hey!!!";
+var name2 = "Kristin";
+
+function callMe (callMe) {
+  var lyric="maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+  
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
+ console.log(crazy);
 }
+
+  var thisIsCrazy = function(){
+  
+}
+
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+  console.log(name);
 }
+
+  function sayMy (name2){
+ 
+  console.log(name2);
+  }
+  
+}
+  
